d159febb4ff6a5f824611c1fec2c8da1250b2dbf
Tassio Med <tassiohfmed@gmail.com>
2022-09-29 14:33:03 -0300

    feat: troca na responsividade, 'sm' por 'md'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,8 @@ function Navbar()  {
     <div  className="w-screen h-20 z-10 bg-zinc-200 fixed drop-shadow-md">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
-          <h1 className="text-3xl font-semibold mr-4 sm:text-4xl">LOGO.</h1>
-          <ul className="hidden sm:flex">
+          <h1 className="text-3xl font-semibold mr-4 md:text-4xl">LOGO.</h1>
+          <ul className="hidden md:flex">
             <li>Home</li>
             <li>About</li>
             <li>Platforms</li>
@@ -20,11 +20,11 @@ function Navbar()  {
             <li>Contact</li>
           </ul>
         </div>
-        <div className="hidden sm:flex pr-4">
+        <div className="hidden md:flex pr-4">
           <button className="border-none bg-transparent text-black mr-4">Sign In</button>
           <button className="px-8 py-3">Sign Up</button>
         </div>
-        <div className="sm:hidden" onClick={handleClick}>
+        <div className="md:hidden" onClick={handleClick}>
           {!navbar ? <HiMenu className="w-5" /> : <GrClose className="w-5" />}
           
         </div>
