693d433001718987f8049e0d13566694d33fcb7a
Tassio Med <tassiohfmed@gmail.com>
2022-09-29 17:03:02 -0300

    feat: botão, imagem e Título do header organizados com responsividade.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import Image from '../images/handPhone.png'
+
 
 function Hero(){
   return(
-    <div>
+    <div className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
+      <div className="grid md:grid-cols-2 max-w-[77.5rem] m-auto">
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-2xl">Lorem ipsum</p>
+          <h1 className="py-3 text-5xl md:text-7xl font-bold">Lorem ipsum</h1>
+          <p className="text-2xl">Lorem ipsum dolor.</p>
+          <button className="py-3 px-6 sm:w-[60%] my-4">Let's start</button>
+        </div>
+        <div>
+          <img src={Image} alt="/" className="w-full"/>
+        </div>
+      </div>
 
     </div>
   )
