b3d29c4c248203298881526951b60838d95dbbdc
Tassio Med <tassiohfmed@gmail.com>
2022-09-28 22:08:46 -0300

    feat: botão de clique e dropdown menu funcionando

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from "react"
+import React, { useState } from 'react';
 
-import { HiMenu } from 'react-icons/hi'
+import { HiMenu } from 'react-icons/hi';
+import { GrClose } from 'react-icons/gr';
 
 function Navbar()  {
+  const [navbar, setNavbar] = useState(false);
+  const handleClick = () => setNavbar(!navbar)
+
   return (
     <div  className="w-screen h-20 z-10 bg-zinc-200 fixed drop-shadow-md">
-      <div className="px-2 flex justify-between items-centerems w-full h-full">
+      <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
           <h1 className="text-3xl font-semibold mr-4 sm:text-4xl">LOGO.</h1>
           <ul className="hidden sm:flex">
@@ -18,11 +22,24 @@ function Navbar()  {
         </div>
         <div className="hidden sm:flex pr-4">
           <button className="border-none bg-transparent text-black mr-4">Sign In</button>
-          <button className="px-8 py-3">Sign In</button>
+          <button className="px-8 py-3">Sign Up</button>
         </div>
-        
+        <div className="sm:hidden" onClick={handleClick}>
+          {!navbar ? <HiMenu className="w-5" /> : <GrClose className="w-5" />}
+          
+        </div>
       </div>
-      <HiMenu className="w-5"/>
+      <ul className={!navbar ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
+        <li className="border-b-2 border-zinc-300 w-full">Home</li>
+        <li className="border-b-2 border-zinc-300 w-full">About</li>
+        <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
+        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+        <li className="border-b-2 border-zinc-300 w-full">Contact</li>
+        <div className="flex flex-col my-4">
+          <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">Sign In</button>
+          <button className="px-8 py-3">Sign Up</button>
+        </div>
+      </ul>
     </div>
   )
 
