8697136d08ab5e26f6b80cb3359ed0d85d31057a
Tassio Med <tassiohfmed@gmail.com>
2022-09-28 21:09:05 -0300

    feat: logo e links posicionados, botões quase prontos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,28 @@
 import React from "react"
 
-import { AiOutlineMenu } from 'react-icons/ai'
+import { HiMenu } from 'react-icons/hi'
 
 function Navbar()  {
   return (
-    <div>
-      <h1>Hello</h1>
-      <AiOutlineMenu className="w-10"/>
-      
+    <div  className="w-screen h-20 z-10 bg-zinc-200 fixed drop-shadow-md">
+      <div className="px-2 flex justify-between items-centerems w-full h-full">
+        <div className="flex items-center">
+          <h1 className="text-3xl font-semibold mr-4 sm:text-4xl">LOGO.</h1>
+          <ul className="hidden sm:flex">
+            <li>Home</li>
+            <li>About</li>
+            <li>Platforms</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="hidden sm:flex pr-4">
+          <button className="border-none bg-transparent text-black mr-4">Sign In</button>
+          <button className="px-8 py-3">Sign In</button>
+        </div>
+        
+      </div>
+      <HiMenu className="w-5"/>
     </div>
   )
 
