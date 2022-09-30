ebc1d6dd607eab907a19046a9fd15769596418fd
Tassio Med <tassiohfmed@gmail.com>
2022-09-30 14:53:33 -0300

    feat: criado componente Team.jsx e importado ao App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About"
+import Team from './components/Team'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Team/>
     </div>
   );
 }
---
+++ src/components/Team.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FiPhone } from "react-icons/fi";
+import { MdDeveloperBoard } from "react-icons/md";
+import { SiMaterialdesignicons } from "react-icons/si";
+import { BsArrowRight } from "react-icons/bs";
+
+function Team(){
+  return (
+    <div></div>
+  )
+}
+
+export default Team;
